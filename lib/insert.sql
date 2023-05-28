ae319c5d727a511e114bbb685ce368faa4c42059
Freddy-ux <alfredmuleri569@gmail.com>
2023-05-28 23:57:30 +0300

    Done With Assignment

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex)
+VALUES
+  ('Mr. Chocolate', 5, 'M'),
+  ('Rowdy', 3, 'M'),
+  ('Tabitha', 4, 'F'),
+  ('Sergeant Brown', 6, 'M'),
+  ('Melissa', 2, 'F'),
+  ('Grinch', 7, 'M'),
+  ('Wendy', 4, 'F'),
+  (NULL, NULL, NULL);
